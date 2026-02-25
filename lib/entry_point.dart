6f2c87056d180218ad6d0bcfd87a406adbd874a1
Lucas Di Blasi <lucas.blasi@grupoboticario.com.br>
2026-02-25 15:52:23 -0300

    action

--- lib/entry_point.dart
+++ lib/entry_point.dart
@@ -79,7 +79,6 @@ class _EntryPointState extends State<EntryPoint> {
           ),
         ],
       ),
-      // body: _pages[_currentIndex],
       body: PageTransitionSwitcher(
         duration: defaultDuration,
         transitionBuilder: (child, animation, secondAnimation) {
